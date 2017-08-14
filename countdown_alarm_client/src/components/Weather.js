f3532a90747426091b112de300559cbcc286734f
Daniel Park <dpeace926@gmail.com>
2017-08-14 18:26:23 -0400

    create Alarm, Weather, WorldClock components

---
+++ countdown_alarm_client/src/components/Weather.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Weather () {
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+export default Weather
---
+++ countdown_alarm_client/src/components/WorldClock.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function WorldClock () {
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+export default WorldClock
