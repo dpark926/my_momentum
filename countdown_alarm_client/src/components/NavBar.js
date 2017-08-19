7bf71b8f3eafe06ce218397a5a70f966f4a3fb20
Daniel Park <dpeace926@gmail.com>
2017-08-19 13:07:34 -0400

    add NavBar styles

--- countdown_alarm_client/src/components/NavBar.js
+++ countdown_alarm_client/src/components/NavBar.js
@@ -1,14 +1,17 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import '../styles/NavBar.css'
 
 function NavBar () {
   return (
-    <div>
-      <Link to="alarm" className='navbar-category'>Alarm</Link>
-      <Link to="weather" className='navbar-category'>Weather</Link>
-      <Link to="worldclock" className='navbar-category'>World Clock</Link>
-      <Link to="pomodoro" className='navbar-category'>Pomodoro</Link>
-      <Link to="stopwatch" className='navbar-category'>Stop Watch</Link>
+    <div className="navbar">
+      <div className="navbar-wrapper">
+        <Link to="alarm" className='navbar-link'><div className="navbar-category">Alarm</div></Link>
+        <Link to="weather" className='navbar-link'><div className="navbar-category">Weather</div></Link>
+        <Link to="worldclock" className='navbar-link'><div className="navbar-category">World Clock</div></Link>
+        <Link to="pomodoro" className='navbar-link'><div className="navbar-category">Pomodoro</div></Link>
+        <Link to="stopwatch" className='navbar-link'><div className="navbar-category">Stop Watch</div></Link>
+      </div>
     </div>
   )
 }
