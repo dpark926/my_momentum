b9bcb79648e2fc2e68cce32dee8501ad958104b2
Daniel Park <dpeace926@gmail.com>
2017-08-19 12:33:54 -0400

    set different time values for Pomodoro

--- countdown_alarm_client/src/containers/Pomodoro.js
+++ countdown_alarm_client/src/containers/Pomodoro.js
@@ -8,35 +8,108 @@ class Pomodoro extends Component {
       start: false,
       hour: 0,
       minute: 0,
-      second: 0,
+      second: 60,
     }
 
-    this.handleStart = this.handleStart.bind(this)
+    this.setAddFive = this.setAddFive.bind(this)
+    this.setSubtractFive = this.setSubtractFive.bind(this)
+    this.startToggle = this.startToggle.bind(this)
+    this.setPomodoro = this.setPomodoro.bind(this)
+    this.setShortBreak = this.setShortBreak.bind(this)
+    this.setLongBreak = this.setLongBreak.bind(this)
+    this.cancelToggle = this.cancelToggle.bind(this)
+    this.secondCounter = this.secondCounter.bind(this)
   }
 
-  handleStart () {
+  setAddFive() {
     this.setState({
-      start: !this.state.start
+      minute: this.state.minute + 5,
+    })
+  }
+
+  setSubtractFive() {
+    this.setState({
+      minute: this.state.minute - 5,
+    })
+  }
+
+  setPomodoro () {
+    this.setState({
+      minute: 25,
+    })
+  }
+
+  setShortBreak () {
+    this.setState({
+      minute: 5,
+    })
+  }
+
+  setLongBreak () {
+    this.setState({
+      minute: 10,
+    })
+  }
+
+  startToggle () {
+    this.setState({
+      start: !this.state.start,
+    })
+    setInterval(this.secondCounter, 1000)
+  }
+
+  cancelToggle () {
+    this.setState({
+      start: !this.state.start,
+      hour: 0,
+      minute: 0,
+      second: 0,
+    })
+    // clearInterval(this.startToggle)
+  }
+
+  secondCounter () {
+    this.setState({
+      second: this.state.second - 1
     })
   }
 
   render () {
     if (!this.state.start) {
-      return (
-        <div>
-          <h1>Pomodoro</h1>
-          <div>{this.state.hour} : {this.state.minute}</div>
-          <div onClick={this.handleStart}>Start</div>
-        </div>
-      )
+      if (this.state.minute === 0) {
+        return (
+          <div>
+            <h1>Pomodoro</h1>
+            <div onClick={this.setSubtractFive}>-</div>
+            <div onClick={this.setAddFive}>+</div>
+            <div>{this.state.hour} : {this.state.minute}</div>
+            <div onClick={this.setPomodoro}>Pomodoro</div>
+            <div onClick={this.setShortBreak}>Short Break</div>
+            <div onClick={this.setLongBreak}>Long Break</div><br/>
+          </div>
+        )
+      } else {
+        return (
+          <div>
+            <h1>Pomodoro</h1>
+            <div onClick={this.setSubtractFive}>-</div>
+            <div onClick={this.setAddFive}>+</div>
+            <div>{this.state.hour} : {this.state.minute}</div>
+            <div onClick={this.setPomodoro}>Pomodoro</div>
+            <div onClick={this.setShortBreak}>Short Break</div>
+            <div onClick={this.setLongBreak}>Long Break</div><br/>
+            <div onClick={this.startToggle}>Start</div>
+          </div>
+        )
+      }
     } else {
       return (
         <div>
           <h1>Pomodoro</h1>
           <div>{this.state.hour} : {this.state.minute} : {this.state.second}</div>
           <div>Pause</div>
-          <div>Resume</div>
-          <div onClick={this.handleStart}>Cancel</div>
+          <div>Resume</div><br/>
+          <div onClick={this.cancelToggle}>Cancel</div>
         </div>
       )
     }
