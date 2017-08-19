6a51ddc00817c206c963bb1caf572e45c84647f4
Daniel Park <dpeace926@gmail.com>
2017-08-19 00:21:32 -0400

    create start/cancel toggle for pomodoro

--- countdown_alarm_client/src/containers/Pomodoro.js
+++ countdown_alarm_client/src/containers/Pomodoro.js
@@ -5,18 +5,41 @@ class Pomodoro extends Component {
     super()
 
     this.state = {
+      start: false,
       hour: 0,
       minute: 0,
+      second: 0,
     }
+
+    this.handleStart = this.handleStart.bind(this)
+  }
+
+  handleStart () {
+    this.setState({
+      start: !this.state.start
+    })
   }
 
   render () {
-    return (
-      <div>
-        <h1>Pomodoro</h1>
-        <div>{this.state.hour} : {this.state.minute}</div>
-      </div>
-    )
+    if (!this.state.start) {
+      return (
+        <div>
+          <h1>Pomodoro</h1>
+          <div>{this.state.hour} : {this.state.minute}</div>
+          <div onClick={this.handleStart}>Start</div>
+        </div>
+      )
+    } else {
+      return (
+        <div>
+          <h1>Pomodoro</h1>
+          <div>{this.state.hour} : {this.state.minute} : {this.state.second}</div>
+          <div>Pause</div>
+          <div>Resume</div>
+          <div onClick={this.handleStart}>Cancel</div>
+        </div>
+      )
+    }
   }
 }
 
