a9cacb7fb3c4184210f739bee07fe833d3ae77af
Daniel Park <dpeace926@gmail.com>
2018-05-10 22:36:14 -0400

    input name for timedate

--- momentum_client/src/containers/TimeDate.js
+++ momentum_client/src/containers/TimeDate.js
@@ -14,6 +14,8 @@ class TimeDate extends Component {
       year: d.getFullYear(),
       hour: d.getHours(),
       minute: d.getMinutes(),
+      name: 'Daniel',
+      showInput: false,
     }
   }
 
@@ -34,6 +36,26 @@ class TimeDate extends Component {
     setInterval(this.countingMinute, 60000);
   }
 
+  switchOffInput = (e) => {
+    if (e.key === 'Enter') {
+      this.setState({
+        showInput: !this.state.showInput
+      })
+    }
+  }
+
+  switchOnInput = () => {
+    this.setState({
+      showInput: !this.state.showInput
+    })
+  }
+
+  handleName = (e) => {
+    this.setState({
+      name: e.target.value
+    })
+  }
+
   render = () => {
     const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "November", "December"]
     const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
@@ -47,15 +69,18 @@ class TimeDate extends Component {
       greeting = "Good Afternoon"
     }
 
+    let input = <input type='text' placeholder='Name' className='inputName' onChange={this.handleName} onKeyPress={this.switchOffInput}></input>
+    let name = <span onClick={this.switchOnInput}>{this.state.name}</span>
+
     return (
       <div className='timedate-wrapper'>
         <div>
+          <div className='timedate-date'>{days[this.state.day]}, {months[this.state.month]} {this.state.date}, {this.state.year}</div>
           <div className='counting-time'>
             {this.state.hour === 0 || this.state.hour === 12 ? "12" : this.state.hour % 12}:{this.state.minute < 10 ? "0" + this.state.minute : this.state.minute}
             <span className="ampm">{this.state.hour < 12 ? "AM" : "PM"}</span>
           </div>
-          <div className='timedate-date'>{days[this.state.day]}, {months[this.state.month]} {this.state.date}, {this.state.year}</div>
-          <div className="alarm-greeting">{greeting}, Daniel</div>
+          <div className="alarm-greeting">{greeting}, {this.state.showInput ? input : name}</div>
         </div>
       </div>
     )
