91599afd64a9501440bcca5aefc6fbb28cee7e7a
Daniel Park <dpeace926@gmail.com>
2018-05-10 21:20:28 -0400

    flexbox timedate comp

--- momentum_client/src/containers/TimeDate.js
+++ momentum_client/src/containers/TimeDate.js
@@ -1,12 +1,12 @@
-import React, {Component} from 'react'
+import React, {Component} from 'react';
 import '../styles/TimeDate.css';
 
 class TimeDate extends Component {
-
   constructor() {
     super()
 
-    let d = new Date()
+    let d = new Date();
+
     this.state = {
       day: d.getDay(),
       month: d.getMonth(),
@@ -18,8 +18,8 @@ class TimeDate extends Component {
   }
 
   countingMinute = () => {
-    let d = new Date()
-    console.log(d)
+    let d = new Date();
+
     this.setState({
       day: d.getDay(),
       month: d.getMonth(),
@@ -31,14 +31,13 @@ class TimeDate extends Component {
   }
 
   componentWillMount = () => {
-    setInterval(this.countingMinute, 60000)
+    setInterval(this.countingMinute, 60000);
   }
 
   render = () => {
     const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "November", "December"]
     const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
     let greeting = 'Hello'
-    console.log(this.state.hour)
 
     if (this.state.hour < 6 || this.state.hour > 17) {
       greeting = "Good Evening"
@@ -50,12 +49,14 @@ class TimeDate extends Component {
 
     return (
       <div className='timedate-wrapper'>
-        <div className='counting-time'>
-          {this.state.hour === 0 || this.state.hour === 12 ? "12" : this.state.hour % 12}:{this.state.minute < 10 ? "0" + this.state.minute : this.state.minute}
-          <span className="ampm">{this.state.hour < 12 ? "AM" : "PM"}</span>
+        <div>
+          <div className='counting-time'>
+            {this.state.hour === 0 || this.state.hour === 12 ? "12" : this.state.hour % 12}:{this.state.minute < 10 ? "0" + this.state.minute : this.state.minute}
+            <span className="ampm">{this.state.hour < 12 ? "AM" : "PM"}</span>
+          </div>
+          <div className='timedate-date'>{days[this.state.day]}, {months[this.state.month]} {this.state.date}, {this.state.year}</div>
+          <div className="alarm-greeting">{greeting}, Daniel</div>
         </div>
-        <div className='timedate-date'>{days[this.state.day]}, {months[this.state.month]} {this.state.date}, {this.state.year}</div>
-        <div className="alarm-greeting">{greeting}, Daniel</div>
       </div>
     )
   }
