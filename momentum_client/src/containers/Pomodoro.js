5cab1dc8ad121f807fffcfbad66d1bdbb095fe87
Daniel Park <dpeace926@gmail.com>
2018-05-15 22:27:11 -0400

    responsive pomodoro comp mobile

--- momentum_client/src/containers/Pomodoro.js
+++ momentum_client/src/containers/Pomodoro.js
@@ -98,7 +98,7 @@ class Pomodoro extends Component {
         <div className="pomodoro">
           <h1>POMODORO</h1>
 
-          <div className="wrapper">
+          <div className="pomodoroNavWrapper">
             <div className="category-box" onClick={this.setPomodoro}>POMODORO</div>
             <div className="category-box" onClick={this.setShortBreak}>SHORT BREAK</div>
             <div className="category-box" onClick={this.setLongBreak}>LONG BREAK</div><br/>
@@ -111,7 +111,7 @@ class Pomodoro extends Component {
             <div className="addSubtract" onClick={this.setAddFive}>+</div><br/>
           </div>
 
-          {(this.state.minute === 0) ? null : <div className="category-box" onClick={this.startToggle}>START</div>}
+          {(this.state.minute === 0) ? <div className="black-category-box"></div> : <div className="category-box" onClick={this.startToggle}>START</div>}
         </div>
       )
     } else {
@@ -123,7 +123,7 @@ class Pomodoro extends Component {
             <div className="pomodoro-time">{Math.floor(((this.state.minute) % 60) / 10)}{(this.state.minute) % 10} : {Math.floor(((this.state.second) % 60) / 10)}{(this.state.second) % 10}</div><br/>
           </div>
 
-          <div className="wrapper">
+          <div className="time-wrapper">
             {(!this.state.pause) ? <div className="category-box" onClick={this.pauseToggle}>PAUSE</div> : <div className="category-box" onClick={this.resumeToggle}>RESUME</div>}
             <div className="category-box" onClick={this.cancelToggle}>CANCEL</div>
           </div>
